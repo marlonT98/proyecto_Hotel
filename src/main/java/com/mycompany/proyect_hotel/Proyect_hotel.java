a655e7ced433b5e927061f304557a6ed51aa1fd1
marlon tarrillo <marlontarrillo19@gmail.com>
2023-11-11 17:54:23 -0500

    Estructura base por ahora

---
+++ src/main/java/com/mycompany/proyect_hotel/Proyect_hotel.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.proyect_hotel;
+
+/**
+ *
+ * @author Marlon
+ */
+public class Proyect_hotel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
